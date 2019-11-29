ec27029afed0307c25e77290fcb21002ee7d923a
Ariel V Lima <arielflima@gmail.com>
2019-11-29 19:46:55 -0300

    change the cart component to use reactHooks with useDispatch and useSelector

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { connect, useSelector } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
@@ -9,15 +8,17 @@ import * as CartActions from '../../store/modules/cart/actions';
 
 import { ProductList } from './styles';
 
-function Home({ addToCartRequest }) {
+export default function Home() {
   const [products, setProducts] = useState([]);
   const amount = useSelector(state =>
-    state.cart.reduce((amount, product) => {
-      amount[product.id] = product.amount;
+    state.cart.reduce((sumAmount, product) => {
+      sumAmount[product.id] = product.amount;
 
-      return amount;
+      return sumAmount;
     }, {})
   );
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     async function loadProducts() {
@@ -34,7 +35,7 @@ function Home({ addToCartRequest }) {
   }, []);
 
   function handleAddProduct(id) {
-    addToCartRequest(id);
+    dispatch(CartActions.addToCartRequest(id));
   }
 
   return (
@@ -58,11 +59,3 @@ function Home({ addToCartRequest }) {
     </ProductList>
   );
 }
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(CartActions, dispatch);
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(Home);

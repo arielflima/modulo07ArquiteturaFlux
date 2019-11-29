b62bd8f7cd452925b8adc066216f04fc4af623d7
Ariel V Lima <arielflima@gmail.com>
2019-11-29 19:33:38 -0300

    change header to useSelector and home too

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
@@ -9,8 +9,15 @@ import * as CartActions from '../../store/modules/cart/actions';
 
 import { ProductList } from './styles';
 
-function Home({ amount, addToCartRequest }) {
+function Home({ addToCartRequest }) {
   const [products, setProducts] = useState([]);
+  const amount = useSelector(state =>
+    state.cart.reduce((amount, product) => {
+      amount[product.id] = product.amount;
+
+      return amount;
+    }, {})
+  );
 
   useEffect(() => {
     async function loadProducts() {
@@ -52,18 +59,10 @@ function Home({ amount, addToCartRequest }) {
   );
 }
 
-const mapStateToProps = state => ({
-  amount: state.cart.reduce((amount, product) => {
-    amount[product.id] = product.amount;
-
-    return amount;
-  }, {}),
-});
-
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
 export default connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps
 )(Home);
